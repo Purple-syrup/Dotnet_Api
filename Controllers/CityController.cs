bd25361c0b29560eb9e35b69ae1eaec8ff697fc7
Purple-syrup <107584822+Purple-syrup@users.noreply.github.com>
2024-03-29 20:08:47 +0000

    create delete city

--- Controllers/CityController.cs
+++ Controllers/CityController.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Dotnet_Api.Data;
+using Dotnet_Api.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Dotnet_Api.Controllers
 {
@@ -18,10 +20,42 @@ namespace Dotnet_Api.Controllers
         {
             _dc = dc;
         }
+
+
         [HttpGet]
-        public IActionResult GetStrings(){
-            var cities =_dc.Cities.ToList();
+        public async Task<IActionResult> GetCities(){
+            var cities = await  _dc.Cities.ToListAsync();
             return Ok(cities);
+        }
+
+      
+        [HttpPost("post")]
+        public async Task<IActionResult> AddCity(City city)
+        {
+            // City city= new City(){Name=cityName};
+            await _dc.Cities.AddAsync(city);
+            await _dc.SaveChangesAsync();
+            return Ok(city);
+        }
+        
+        [HttpPost("add")]
+        [HttpPost("add/{cityName}")]
+        public async Task<IActionResult> AddCity(string cityName)
+        {
+            City city= new City(){Name=cityName};
+            await _dc.Cities.AddAsync(city);
+            await _dc.SaveChangesAsync();
+            return Ok(city);
+        }
+
+        [HttpDelete("delete/{id}")]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCity(int id)
+        {
+            var city= await _dc.Cities.FindAsync(id);
+            _dc.Cities.Remove(city);
+            await _dc.SaveChangesAsync();
+            return Ok(city);
         }
     }
 }
